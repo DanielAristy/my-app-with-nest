55c429ed8a07e827ce99727587713ca80674db3a
Daniel Aristizabal <daniel.aristizabal@sofka.com.co>
2022-11-11 18:58:55 -0500

    Se realiza documentacion con swagger

--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -3,19 +3,28 @@ import { AuthGuard } from '../auth.guard';
 import { UserDto } from '../dtos/user.dto/user.dto';
 import { Interceptor } from '../response.interceptor';
 import { UserService } from '../services/user.service';
+import { ApiBearerAuth, ApiBody, ApiForbiddenResponse, ApiResponse, ApiTags, getSchemaPath } from '@nestjs/swagger';
 
+@ApiTags('user')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
   @UseInterceptors(Interceptor)
+  @ApiResponse({ status: 200, type: [UserDto] })
   getAll() {
     return this.userService.findAll();
   }
 
   @Get(':uuid')
   @UseInterceptors(Interceptor)
+  @ApiResponse({
+    status: 200,
+    schema: {
+      $ref: getSchemaPath(UserDto),
+    },
+  })
   findById(@Param('uuid') uuid: string){
     return this.userService.findById(uuid)
   }
@@ -23,6 +32,12 @@ export class UserController {
   @Post()
   @UseInterceptors(Interceptor)
   @UseGuards(AuthGuard)
+  @ApiResponse({
+    status: 201,
+    type: UserDto,
+  })
+  @ApiForbiddenResponse({ description: 'Forbidden.'})
+  @ApiBearerAuth()
   createUser(
     @Body(
       new ValidationPipe({
@@ -39,6 +54,13 @@ export class UserController {
   @Put(':uuid')
   @UseGuards(AuthGuard)
   @UseInterceptors(Interceptor)
+  @ApiBody({ type: UserDto })
+  @ApiResponse({
+    status: 200,
+    type: UserDto,
+  })
+  @ApiForbiddenResponse({ description: 'Forbidden.'})
+  @ApiBearerAuth()
   updateUser(
     @Param('uuid') uuid: string,
     @Body(
@@ -56,6 +78,13 @@ export class UserController {
   @Patch(':uuid')
   @UseGuards(AuthGuard)
   @UseInterceptors(Interceptor)
+  @ApiBody({ type: UserDto })
+  @ApiResponse({
+    status: 200,
+    type: UserDto,
+  })
+  @ApiForbiddenResponse({ description: 'Forbidden.'})
+  @ApiBearerAuth()
   modifyUser(@Param('uuid') uuid: string,
   @Body(
     new ValidationPipe({
@@ -71,6 +100,8 @@ export class UserController {
 
   @Delete(':uuid')
   @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  @ApiForbiddenResponse({ description: 'Forbidden.'})
   deleteUser(@Param('uuid') uuid: string) {
     return this.userService.deleteUser(uuid)
   }
--- src/user/dtos/user.dto/user.dto.ts
+++ src/user/dtos/user.dto/user.dto.ts
@@ -1,19 +1,26 @@
 import { User } from "src/user/interfaces/user/user.interface";
 import { IsNotEmpty, IsOptional, IsString, IsUUID } from 'class-validator';
 import { v4 as uuid } from 'uuid';
+import { ApiProperty, ApiPropertyOptional, PartialType } from "@nestjs/swagger";
 
 export class UserDto implements User {
     @IsUUID()
     @IsOptional()
+    @ApiProperty({description: "Se auto genera, no es necesario enviarlo"})
+    @ApiPropertyOptional()
     uuid: string;
     @IsString()
     @IsNotEmpty()
+    @ApiProperty({description: "Nombre de la persona"})
     name: string;
     @IsString()
     @IsOptional()
+    @ApiProperty({description: "Apellido de la persona"})
+    @ApiPropertyOptional()
     surname?: string;
     @IsString()
     @IsNotEmpty()
+    @ApiProperty({description: "Correo personal"})
     email: string;
 
     constructor(user?: User){
@@ -23,3 +30,5 @@ export class UserDto implements User {
         this.email = user?.email ?? '';
     }
 }
+
+export class UpdateUserDto extends PartialType(UserDto){}
